779ab47fadf8f7e5f5ef954a3148b94c1da85252
Anyelo Pulido <anyelo24353209@gmail.com>
2022-03-23 01:37:29 -0400

    Add files via upload
    tarea 2

---
+++ tareas/asignacion_02.py
@@ -0,0 +1,27 @@
+""" 
+4.1 Implemente una tupla de un solo elemento. Tiene una particularidad, investigue. (2pts.)
+
+4.2 Dada una lista de las edades de una población que ha ido a vacunarse,
+
+vEdades = [2, 7, 58, 7, 45, 26, 10, 8, 56, 57, 97, 19, 11, 53, 3, 99, 62, 78, 29, 9, 37, 42, 56, 86, 28, 86, 95, 26, 49, 67, 21, 815, 67, 10, 58, 512, 24, 92, 89, 67, 53, 10, 9, 83, 1, 44, 10, 77, 98, 73, 57]
+
+Elimine de la lista, todas las ocurrencias del entero 10 (3pts)
+
+Fecha tope de entrega: lunes 28 de Marzo. 4:30pm.
+"""
+
+
+#1 tupla de un solo elemento. Debe tener coma(,).
+vlenguaje = ('Python',)
+
+
+
+print(vlenguaje)
+
+#2 Elimine de la lista, todas las ocurrencias del entero 10 (con "remove" se elimina la ocurrencia del valor.)
+vEdades = [2, 7, 58, 7, 45, 26, 10, 8, 56, 57, 97, 19, 11, 53, 3, 99, 62, 78, 29, 9, 37, 42, 56, 86, 28, 86, 95, 26, 49, 67, 21, 815, 67, 10, 58, 512, 24, 92, 89, 67, 53, 10, 9, 83, 1, 44, 10, 77, 98, 73, 57]
+
+
+vEdades.remove(10,)
+
+print("sin el 10",vEdades)
