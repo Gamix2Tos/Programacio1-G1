af77578175804d6a755998229675d764ecb5ca6d
Anyelo Pulido <anyelo24353209@gmail.com>
2022-03-25 12:53:28 -0400

    Update asignacion_02.py
    luego de verificar bien me di cuenta que "vEdades.remove(10,)" solo borraba 1, por lo que lei mas y practique hasta llegar a lo modificado. solo que aun con la duda si dejar un 10 o borrar todos. puede que lo modifique nuevamente para comentar el proceso dejando un solo 10

--- tareas/asignacion_02.py
+++ tareas/asignacion_02.py
@@ -20,8 +20,19 @@ print(vlenguaje)
 
 #2 Elimine de la lista, todas las ocurrencias del entero 10 (con "remove" se elimina la ocurrencia del valor.)
 vEdades = [2, 7, 58, 7, 45, 26, 10, 8, 56, 57, 97, 19, 11, 53, 3, 99, 62, 78, 29, 9, 37, 42, 56, 86, 28, 86, 95, 26, 49, 67, 21, 815, 67, 10, 58, 512, 24, 92, 89, 67, 53, 10, 9, 83, 1, 44, 10, 77, 98, 73, 57]
+vdel = 10
 
-
+for vocu in vEdades:
+	if(vocu==vdel):
+		vEdades.remove(vdel)
+"""
+otra opcion es por cada uno de los enteros 10 a eliminar pero seria poco practico si no conoces la cantidad.
 vEdades.remove(10,)
+vEdades.remove(10,)
+vEdades.remove(10,)
+vEdades.remove(10,)
+
+"""
+
 
 print("sin el 10",vEdades)
